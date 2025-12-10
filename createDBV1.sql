0758f725d079fd1aaa34381b4f0250f924cae228
umarraheem123 <b00152481@mytudublin.ie>
2025-12-10 03:55:32 +0000

    added foreign key and fixed database query

--- createDBV1.sql
+++ createDBV1.sql
@@ -2,16 +2,24 @@ DROP DATABASE IF EXISTS gym_capacity;
 CREATE DATABASE gym_capacity;
 USE gym_capacity;
 
+CREATE TABLE members (
+    member_id INT AUTO_INCREMENT PRIMARY KEY,
+    member_name VARCHAR(100) NOT NULL
+);
+
+
+CREATE TABLE attendance_log (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    member_id INT NOT NULL,
+    action ENUM('check_in', 'check_out') NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (member_id) REFERENCES members(member_id)
+);
+
+
 CREATE TABLE attendance (
     id INT AUTO_INCREMENT PRIMARY KEY,
     action ENUM('enter','exit') NOT NULL,
     event_time DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-
-CREATE TABLE attendance_log (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    member_name VARCHAR(100) NOT NULL,
-    action ENUM('check_in', 'check_out') NOT NULL,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-);
